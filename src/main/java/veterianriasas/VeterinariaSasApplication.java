6912483c8868b04215e6a0c33cb004d0323dd751
ortizortix <acocogollo4@gmail.com>
2023-01-30 21:45:33 -0500

    first commit

---
+++ src/main/java/veterianriasas/VeterinariaSasApplication.java
@@ -0,0 +1,13 @@
+package veterianriasas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeterinariaSasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeterinariaSasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/veterianriasas/VeterinariaSasApplicationTests.java
@@ -0,0 +1,13 @@
+package veterianriasas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeterinariaSasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
